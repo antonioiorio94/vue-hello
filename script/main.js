ac8f036b3e53e0e67785240acf92500afc638136
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-24 15:48:27 +0100

    aggiunta testo nell'html

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
-alert("js ok!");
+const root = new Vue({
+  el: "#root",
+  data: {
+    firstName: "Homer",
+    lastName: "Simpson",
+    role: "Chemist and safety inspector",
+    address: "742 Evergreen Terrace, Springfield, United States",
+  },
+});
